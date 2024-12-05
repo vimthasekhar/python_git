9ff22edea7669304a19bfc87199cbf3a0e9516ec
vimthasekhar <vimthasekhar@gmail.com>
2024-12-05 07:10:05 +0530

    second commit

--- basics/example1.py
+++ basics/example1.py
@@ -1,4 +1,20 @@
-print(10+5)
-print(10-5)
-print(10*5)
-print(10/5)
+def add():
+    print(10 + 5)
+
+
+def sub():
+    print(10 - 5)
+
+
+def mul():
+    print(10 * 5)
+
+
+def division():
+    print(10 / 5)
+
+
+add()
+sub()
+mul()
+division()
